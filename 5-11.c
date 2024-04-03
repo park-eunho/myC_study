5c16b6fdee7c5a79db3424b1a1ae8bc1e76c6b43
park-eunho <162663866+park-eunho@users.noreply.github.com>
2024-04-03 16:52:14 +0900

    Add files via upload

---
+++ 5-11.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+void main()
+{
+	int a,b,result;
+	char c;
+	
+
+	printf("수식을 한줄로 띄어쓰기로 입력하세요 : ");
+	scanf("%d %c %d* ", &a, &c, &b);
+
+	switch (c) {
+		case '+' :
+			  result = a + b;
+			  printf("%d %c %d = %d 입니다.", a, c, b, result);
+			  break;
+
+		case '-' :
+			  result = a - b;
+			  printf("%d %c %d = %d 입니다.", a, c, b, result);
+			  
+			  break;
+		case '*' :
+			 result = a * b;
+			 printf("%d %c %d = %d 입니다.", a, c , b, result);
+			 	
+			 break;
+	       		
+      		case '/' :
+			 result = a / b;
+			 printf("%d %c %d = %d 입니다.", a, c, b, result);
+			  break;
+
+	        case '%' :
+			  result = a % b;
+			  printf("%d %c %d = %d 입니다.", a, c, b, result);
+			  break;
+
+		default :
+			 printf("연산자를 잘못 입력했습니다.");	  
+			 break;
+	}
+}
